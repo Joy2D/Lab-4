bfb5def9ab576b22ab5d8b24ffd68a1100235472
Joy2D <Xixexi!7>
2017-09-27 16:38:26 -0400

    This just randomizes a number that shows the reality of what "Dat Boi" is going through. If the number is less than 10 then "Dat Boi" is flying. If it's not then "Dat Boi" is just isn't about that life.

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a =  (int) (Math.random()*20);
+
+        if (a < 10 ){
+            System.out.println("Dat boi 'boutta fly with dem wings");
+        } else if(a > 10) {
+            System.out.println("Dat boi ain't really 'bout it");
+        }
+
+    }
+}
